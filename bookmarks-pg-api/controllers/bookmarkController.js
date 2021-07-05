6ba629fcb489b49046977b1fe709023124d92648
Grace Bong <gracebong.park@gmail.com>
2021-07-05 17:12:18 -0400

    queries

--- bookmarks-pg-api/controllers/bookmarkController.js
+++ bookmarks-pg-api/controllers/bookmarkController.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bookmarks = express.Router();
+const { getAllBookmarks } = require('../queries/bookmarks');
 
-bookmarks.get('/', (req, res) => {
-    res.json({status: 'ok'});
+bookmarks.get('/', async (req, res) => {
+    const allBookmarks = await getAllBookmarks();
+    res.json(allBookmarks);
 });
 
 module.exports = bookmarks;
