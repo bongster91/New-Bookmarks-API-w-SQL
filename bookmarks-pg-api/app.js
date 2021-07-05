1b0813cf28527cb38c7878a69c13a90e94b83d02
Grace Bong <gracebong.park@gmail.com>
2021-07-05 16:49:31 -0400

    sql and pg-promise in db

--- bookmarks-pg-api/app.js
+++ bookmarks-pg-api/app.js
@@ -1,13 +1,19 @@
 const cors = require('cors');
 const express = require('express');
+const bookmarksController = require('./controllers/bookmarkController');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/bookmarks', bookmarksController);
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Bookmarks App!');
+});
+
+app.get('*', (req, res) => {
+    res.status(404).send('Page not found.');
 });
 
 module.exports = app;
